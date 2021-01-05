1cfdb813adda153ba6350fc36182da05343a8fc5
Gavin Day <mengda@leeds.ac.uk>
2021-01-05 11:34:45 +0000

    tracking of circles working

--- circle.py
+++ circle.py
@@ -57,6 +57,73 @@ def drawCirc(x, y, r):
     cv2.imshow("output", img)
     cv2.waitKey(1)
 
+def find(needle_img, haystack_img, threshold=0.5, debug_mode=None,
+        method=cv2.TM_CCOEFF_NORMED):
+    needle_w = needle_img.shape[1]
+    needle_h = needle_img.shape[0]
+    # run the OpenCV algorithm
+    result = cv2.matchTemplate(haystack_img, needle_img, method)
+
+    # Get the all the positions from the match result that exceed our threshold
+    locations = np.where(result >= threshold)
+    locations = list(zip(*locations[::-1]))
+    #print(locations)
+
+    # You'll notice a lot of overlapping rectangles get drawn. We can eliminate those redundant
+    # locations by using groupRectangles().
+    # First we need to create the list of [x, y, w, h] rectangles
+    rectangles = []
+    for loc in locations:
+        rect = [int(loc[0]), int(loc[1]), needle_w, needle_h]
+        # Add every box to the list twice in order to retain single (non-overlapping) boxes
+        rectangles.append(rect)
+        rectangles.append(rect)
+    # Apply group rectangles.
+    # The groupThreshold parameter should usually be 1. If you put it at 0 then no grouping is
+    # done. If you put it at 2 then an object needs at least 3 overlapping rectangles to appear
+    # in the result. I've set eps to 0.5, which is:
+    # "Relative difference between sides of the rectangles to merge them into a group."
+    rectangles, weights = cv2.groupRectangles(rectangles, groupThreshold=1, eps=0.5)
+    #print(rectangles)
+
+    points = []
+    if len(rectangles):
+        #print('Found needle.')
+
+        line_color = (0, 255, 0)
+        line_type = cv2.LINE_4
+        marker_color = (255, 0, 255)
+        marker_type = cv2.MARKER_CROSS
+
+        # Loop over all the rectangles
+        for (x, y, w, h) in rectangles:
+
+            # Determine the center position
+            center_x = x + int(w/2)
+            center_y = y + int(h/2)
+            # Save the points
+            points.append((center_x, center_y))
+
+            if debug_mode == 'rectangles':
+                # Determine the box position
+                top_left = (x, y)
+                bottom_right = (x + w, y + h)
+                # Draw the box
+                cv2.rectangle(haystack_img, top_left, bottom_right, color=line_color, 
+                            lineType=line_type, thickness=2)
+            elif debug_mode == 'points':
+                # Draw the center point
+                cv2.drawMarker(haystack_img, (center_x, center_y), 
+                            color=marker_color, markerType=marker_type, 
+                            markerSize=40, thickness=2)
+
+    if debug_mode:
+        cv2.imshow('Matches', haystack_img)
+        #cv.waitKey()
+        #cv.imwrite('result_click_point.jpg', haystack_img)
+
+    return points
+
 def getScr():
     img = ImageGrab.grab(bbox=(50, 100,960,900))
     #910x 800
@@ -70,12 +137,24 @@ def getScr():
 
     return img
 
+def toGray(img):
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    img_gray = cv2.GaussianBlur(img_gray, (7, 7), 0)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    return gray
+
 try:
     #pyautogui.moveTo(460, 510, duration=0.25)
     #circle(mult=100) 
     loop_time = time.time()
     while True:
-        cv2.imshow("out", getScr())
+        #cv2.imshow("out", getScr())
+        needle = cv2.imread("blob.png",cv2.IMREAD_UNCHANGED)
+        #needle = toGray(needle)
+        find(needle, getScr(),0.5,debug_mode='rectangles')
+
         print('FPS {}'.format(1 / (time.time() - loop_time)))
         loop_time = time.time()
 
