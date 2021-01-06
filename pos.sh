746b3f8ddffea6fc92a11a3a1ab30413dab230f6
Gavin Day <mengda@leeds.ac.uk>
2021-01-06 18:38:41 +0000

    tidying

--- pos.sh
+++
@@ -1,6 +0,0 @@
-while read line; do
-     X=`echo $line| cut -c1-3`; 
-     Y=`echo $line| cut -c4-7`;
-     xdotool mousemove --sync $((  0.5 + $X )) $(( 0.5 + $Y ));
-     xdotool click 1
-done < positions.txt
