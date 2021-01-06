a04184daa881eaa5db6546447968589606d048f2
Zach Welshman <zmswelshman@gmail.com>
2021-01-06 13:32:56 +0000

    new slither

--- src/slither.py
+++ src/slither.py
@@ -5,46 +5,21 @@ from PIL import ImageGrab
 import numpy as np
 import imutils
 
-''' 
-It is good practice to add doc strings to functions explain what they do
-Also defining the data type of the parameters that it takes and returns
 
-eg.
-
-def multiply_a_b( parameter_a : int, parameter_b : float) -> float:
-    return parameter_a * parameter_b
-
- '''
 
 def circle(mult):
-    """
-
-    Doc string explaining what the function does
-
-    example usage
-
-
-    """
     while True:
-        pyautogui.moveRel(mult * 1, mult * -1, duration=dur)
-        pyautogui.moveRel(mult * 1, 0, duration=dur)
-        pyautogui.moveRel(mult * 1, mult * 1, duration=dur)
-        pyautogui.moveRel(0, mult * 1, duration=dur)
-        pyautogui.moveRel(mult * -1, mult * 1, duration=dur)
-        pyautogui.moveRel(mult * -1, 0, duration=dur)
-        pyautogui.moveRel(mult * -1, mult * -1, duration=dur)
-        pyautogui.moveRel(0, mult * -1, duration=dur)
+        pyautogui.moveRel(mult*1, mult*-1, duration=dur)
+        pyautogui.moveRel(mult*1, 0, duration=dur)
+        pyautogui.moveRel(mult*1, mult*1, duration=dur)
+        pyautogui.moveRel(0, mult*1,duration=dur)
+        pyautogui.moveRel(mult*-1, mult*1,duration=dur)
+        pyautogui.moveRel(mult*-1, 0,duration=dur)
+        pyautogui.moveRel(mult*-1, mult*-1,duration=dur)
+        pyautogui.moveRel(0, mult*-1,duration=dur)
 
-
-def hough(img: np.array):
-    """
-    Doc string explaining what the function does
-
-    example usage
-
-    """
-
-    # resized = imutils.resize(img, width=1000)
+def hough(img):
+        #resized = imutils.resize(img, width=1000)
     # convert the resized image to grayscale, blur it slightly,
     # and threshold it
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -52,16 +27,8 @@ def hough(img: np.array):
     img_gray = cv2.GaussianBlur(img_gray, (7, 7), 0)
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    circles = cv2.HoughCircles(
-        img_gray,
-        cv2.HOUGH_GRADIENT,
-        1.1,
-        minDist=15,
-        param1=50,
-        param2=18,
-        minRadius=1,
-        maxRadius=22,
-    )
+    circles = cv2.HoughCircles(img_gray, cv2.HOUGH_GRADIENT, 1.1, minDist=15,
+                     param1=50, param2=18, minRadius=1, maxRadius=22)
 
     # ensure at least some circles were found
     if circles is not None:
@@ -70,50 +37,26 @@ def hough(img: np.array):
         # loop over the (x, y) coordinates and radius of the circles
         for (x, y, r) in circles:
             dist = distFrom(x, y)
+            
+            if(dist < 20000 and dist > 10000):
+                pyautogui.moveTo(x+50, y+100, duration=0)
+                #drawCirc(x, y, r)
 
-            if dist < 20000 and dist > 10000:
-                pyautogui.moveTo(x + 50, y + 100, duration=0)
-                # drawCirc(x, y, r)
 
-'''
-In python Cammel casing is typically reserved for classes
-
-functions generally have lower case function definitions, other languages may differ
-
-e.g. 
-def distance_from_center()
-
-'''
-
-def distFrom(x, y):
+def distFrom(x,y):
     centrex = 475
     centrey = 462
-    return ((x - centrex) * (x - centrex)) + ((y - centrey) * (y - centrey))
-
-
-
+    return (((x - centrex)*(x-centrex)) + ((y-centrey)*(y-centrey)))
+            
 def drawCirc(x, y, r):
     cv2.circle(img, (x, y), r, (0, 255, 255), 4)
     cv2.rectangle(img, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
-    # show the output image
+            # show the output image
     cv2.imshow("output", img)
     cv2.waitKey(1)
 
-'''
-Functions could have better names
-e.g.
-
-def find_snek()
-
-'''
-
-def find(
-    needle_img,
-    haystack_img,
-    threshold=0.5,
-    debug_mode=None,
-    method=cv2.TM_CCOEFF_NORMED,
-):
+def find(needle_img, haystack_img, threshold=0.5, debug_mode=None,
+        method=cv2.TM_CCOEFF_NORMED):
     needle_w = needle_img.shape[1]
     needle_h = needle_img.shape[0]
     # run the OpenCV algorithm
@@ -122,7 +65,7 @@ def find(
     # Get the all the positions from the match result that exceed our threshold
     locations = np.where(result >= threshold)
     locations = list(zip(*locations[::-1]))
-    # print(locations)
+    #print(locations)
 
     rectangles = []
     for loc in locations:
@@ -141,7 +84,7 @@ def find(
 
     points = []
     if len(rectangles):
-        # print('Found needle.')
+        #print('Found needle.')
 
         line_color = (0, 255, 0)
         line_type = cv2.LINE_4
@@ -152,143 +95,153 @@ def find(
         for (x, y, w, h) in rectangles:
 
             # Determine the center position
-            center_x = x + int(w / 2)
-            center_y = y + int(h / 2)
+            center_x = x + int(w/2)
+            center_y = y + int(h/2)
             points.append((center_x, center_y))
 
-            if debug_mode == "rectangles":
+            if debug_mode == 'rectangles':
                 # Determine the box position
                 top_left = (x, y)
                 bottom_right = (x + w, y + h)
-                cv2.rectangle(
-                    haystack_img,
-                    top_left,
-                    bottom_right,
-                    color=line_color,
-                    lineType=line_type,
-                    thickness=2,
-                )
-            elif debug_mode == "points":
-                cv2.drawMarker(
-                    haystack_img,
-                    (center_x, center_y),
-                    color=marker_color,
-                    markerType=marker_type,
-                    markerSize=40,
-                    thickness=2,
-                )
+                cv2.rectangle(haystack_img, top_left, bottom_right, color=line_color, 
+                            lineType=line_type, thickness=2)
+            elif debug_mode == 'points':
+                cv2.drawMarker(haystack_img, (center_x, center_y), 
+                            color=marker_color, markerType=marker_type, 
+                            markerSize=40, thickness=2)
 
-    # if debug_mode:
-    # cv2.imshow('Matches', haystack_img)
-    # cv.waitKey()
-    # cv.imwrite('result_click_point.jpg', haystack_img)
+    #if debug_mode:
+        #cv2.imshow('Matches', haystack_img)
+        #cv.waitKey()
+        #cv.imwrite('result_click_point.jpg', haystack_img)
 
     return haystack_img, points
 
-
 def getScr():
-    img = ImageGrab.grab(bbox=(19, 91, 970, 1015))
-    # 910x 800
+    img = ImageGrab.grab(bbox=(19, 91,970,1015))
+    #910x 800
     # make image C_CONTIGUOUS to avoid errors that look like:
     #   File ... in draw_rectangles
     #   TypeError: an integer is required (got type tuple)
     # see the discussion here:
     # https://github.com/opencv/opencv/issues/14866#issuecomment-580207109
     img = np.ascontiguousarray(img)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
+    # This makes the colours "normal" but costs ~1 fps
 
     return img
-
 
 def toGray(img):
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    img_gray = cv2.GaussianBlur(img_gray, (7, 7), 0)
+    #img_gray = cv2.GaussianBlur(img_gray, (7, 7), 0)
 
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    return gray
+    return img_gray
 
-'''
-Do sm and cp need to be hard coded?
-Also i dont think sm and smolPoint are both needed
-
-maybe better to have it like:
-
-def closestPoint(points, smolPoint, cp):
-    smolPoint=100000
-    cp=2000
-
-
-'''
+def toCol(img):
+    img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+    return img
 
 def closestPoint(points, sm=100000, cp=200):
 
     smolPoint = sm
-    clP = (0, 0)
+    clP = (0,0)
     for point in points:
-        # point = np.round(point).astype("int")
-        p = distFrom(point[0], point[1])
+        #point = np.round(point).astype("int")
+        p = distFrom(point[0],point[1])
         if p < smolPoint and p > cp:
             smolPoint = p
-            clP = (point[0], point[1])
-            # print(p)
+            clP = (point[0],point[1])
+            #print(p)
     return clP
+
+def colourThreshold(img):
+    #colour order is blue, green, red
+    #img = cv2.GaussianBlur(img, (7, 7), 0)
+
+    lower_color_bounds = (0, 0, 40)
+    upper_color_bounds = (255,255,130)
+    mask = cv2.inRange(img,lower_color_bounds,upper_color_bounds )
+    return mask 
+
+def findSnakeContours(thresholdedimg, ogimg, midw, midh):
+
+    img = thresholdedimg
+    
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    largecont = []
+    numcont = 0
+    rectarr = []
+    centers = []
+    for c in contours:
+        if( cv2.contourArea(c) > 500):
+            x,y,w,h = cv2.boundingRect(c)
+            cent = (x+w/2,y+h/2)
+            dist = ((cent[0]-midw)**2 + (cent[1]-midh)**2)
+            #print(dist)
+            if dist < 50000 and dist > 2000:
+                largecont.append(c)
+                numcont+=1
+
+                #centers.append((x+((w-x)/2),y+((h-y)/2)))
+                centers.append(cent)
+                #img = cv2.rectangle(ogimg,(x,y),(x+w,y+h),(0,255,0),2)
+    #print(centers)
+    #img = cv2.drawContours(ogimg, largecont, -1, (0,255,0), 3)
+    return img, centers
 
 
 try:
-    midwidth = int(getScr().shape[1] / 2)
-    midheight = int(getScr().shape[0] / 2)
-    captEdgeDistX = 19
-    captEdgeDistY = 91
+    midwidth = int(getScr().shape[1]/2)
+    midheight = int(getScr().shape[0]/2)
+    captEdgeDistX = 19 #for mouse movements
+    captEdgeDistY = 91 #for mouse movements
     marker_color = (255, 0, 255)
     marker_type = cv2.MARKER_CROSS
-    # pyautogui.moveTo(460, 510, duration=0.25)
-    # circle(mult=100)
+    #pyautogui.moveTo(460, 510, duration=0.25)
+    #circle(mult=100) 
     loop_time = time.time()
-    offset = (midheight - 200, midwidth - 200)
-    needle = cv2.imread("blob.png", cv2.IMREAD_UNCHANGED)
+    offset=(midheight - 200,midwidth- 200)
+    needle = cv2.imread("blob.png",cv2.IMREAD_UNCHANGED)
+
 
     while True:
         ogimg = getScr()
-        img, points = find(needle, ogimg, 0.65, debug_mode="rectangles")
-        clp = closestPoint(points)
-        if clp != (0, 0):
-            # _pause defaults to true, creates large slowdown
-            pyautogui.moveTo(
-                clp[0] + captEdgeDistX, clp[1] + captEdgeDistY, _pause=False
-            )
+        img, points = find(needle, (ogimg),0.65,debug_mode='rectangles')
+        clp = closestPoint(points,sm=10000, cp=400)
 
-            cv2.drawMarker(
-                img,
-                (clp[0], clp[1]),
-                color=marker_color,
-                markerType=marker_type,
-                markerSize=40,
-                thickness=2,
-            )
+        contimg, cent = findSnakeContours(colourThreshold(img),ogimg, midwidth,
+                midheight)
+
+        for c in cent:
+            cv2.drawMarker(img, (int(c[0]),int(c[1])), 
+                           color=(0,0,255), markerType=marker_type, 
+                           markerSize=40, thickness=20)
+        if (clp != (0, 0)):
+            # _pause defaults to true, creates large slowdown
+            pyautogui.moveTo(clp[0]+captEdgeDistX, 
+                             clp[1]+captEdgeDistY, _pause=False)
+
+            cv2.drawMarker(img, (clp[0],clp[1]), 
+                           color=marker_color, markerType=marker_type, 
+                           markerSize=40, thickness=2)
         else:
-            pyautogui.moveTo(
-                midwidth + captEdgeDistX, midheight + captEdgeDistY, _pause=False
-            )
+            pyautogui.moveTo(midwidth + captEdgeDistX,
+                             midheight + captEdgeDistY, _pause=False) 
             # _pause defaults to true, creates large slowdown
         # centre marker
-        cv2.drawMarker(
-            img,
-            (midwidth, midheight),
-            color=(55, 255, 255),
-            markerType=marker_type,
-            markerSize=40,
-            thickness=5,
-        )
-
-        print("FPS {}".format(1 / (time.time() - loop_time)))
+        cv2.drawMarker(img, (midwidth,
+            midheight), color=(55,255,255), markerType=marker_type,
+            markerSize=40, thickness=1)
+        
+        print('FPS {}'.format(1 / (time.time() - loop_time)))
         loop_time = time.time()
         cv2.imshow("out", img)
-        cv2.moveWindow("out", 950, 20)
-        # cv2.imshow("out2", thresh)
-        # cv2.moveWindow("out2", 950,200)
-        if cv2.waitKey(1) == ord("q"):
+        cv2.moveWindow("out", 950,20)
+        #cv2.imshow("out2", contimg)
+       # cv2.moveWindow("out2", 950,200)
+        if cv2.waitKey(1) == ord('q'):
             cv2.destroyAllWindows()
             break
 except KeyboardInterrupt:
-    print("done")
+    print('done')
