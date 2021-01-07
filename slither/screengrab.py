9cbeb06bb07021b4d7f31e2c2ed243a4d941cfb8
Gavin Day <mengda@leeds.ac.uk>
2021-01-07 18:54:54 +0000

    docstrings for screengrab

--- slither/screengrab.py
+++ slither/screengrab.py
@@ -4,10 +4,16 @@ import numpy as np
 
 class ScreenGrab:
     def __init__(self):
+        """Class contains the grabbing the image and most of the image
+        manipulation. Calls self.extent() and self.get_scr() on init"""
+
         self.extent = self.open_conf()
         self.img = self.get_scr() 
         
     def open_conf(self):
+        """Opens the config file to read the capture dimensions. Returns
+        a tuple containing the four corners of the capture area"""
+
         f = open("../capture_size.conf","r")
         bbox = f.readline()
         f.close()
@@ -15,11 +21,12 @@ class ScreenGrab:
         return (bbox)
 
     def get_scr(self):
+        """Grabs the image based on the extent function. Returns the image as
+        an RGB (cv2 defaults to BGR)"""
 
         img = ImageGrab.grab(bbox=self.extent)
-        # 910x 800
+        
         # make image C_CONTIGUOUS to avoid errors that look like:
-        #   File ... in draw_rectangles
         #   TypeError: an integer is required (got type tuple)
         # see the discussion here:
         # https://github.com/opencv/opencv/issues/14866#issuecomment-580207109
@@ -31,6 +38,8 @@ class ScreenGrab:
 
 
     def toGray(self):
+        """Takes self.img and returns a grayscale image"""
+
         img_gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
 
         #img_gray = cv2.GaussianBlur(img_gray, (7, 7), 0)
@@ -39,10 +48,17 @@ class ScreenGrab:
 
 
     def to_col(self):
+        """Takes a grayscale image and returns and colour image - useful for
+        drawing colour on a gray image"""
+
         img = cv2.cvtColor(self.img, cv2.COLOR_GRAY2RGB)
         return img
 
     def colour_threshold(self):
+        """Takes the self.img and applied two inrange filters (the second one
+        is for the colour orange...) and combines them, returns the combined
+        image. Used for the contour detection"""
+
         # colour order is blue, green, red
         #img = cv2.GaussianBlur(img, (7, 7), 0)
         lower_color_bounds = (10, 10, 40)
